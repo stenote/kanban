b4edf0c7f01ed118e365921427deea4e2c50ded6
Rui Ma <xjmarui@gmail.com>
2016-12-04 23:25:25 +0800

    列表页面简单开发

--- kanban/controllers/main.py
+++ kanban/controllers/main.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+from kanban.models import Repo
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', **{
+        'Repos': Repo.query.all()
+    })
